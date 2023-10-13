95d0fcd48e72ebcf9b7a6a4554a594f5e269e377
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2023-10-13 19:37:11 +0500

    First Line

---
+++ 01basic/src/MyFirstLine.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyFirstLine = () => {
+  return (
+    <div>
+        <h1>
+            My New Journey Of React
+        </h1>
+    </div>
+  )
+}
+
+export default MyFirstLine
