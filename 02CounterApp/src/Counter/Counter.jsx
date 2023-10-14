f358ae1c08e5675037b5f4b271c94446ea5545ee
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2023-10-14 20:54:42 +0500

    We create counter using Hooks useState

---
+++ 02CounterApp/src/Counter/Counter.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    let [counter , setCounter] = useState(0)
+
+
+    const Adding = () => {
+        setCounter(counter + 1)
+    }
+    const Removing = () => {
+        setCounter(counter - 1)
+    }
+  return (
+    <div>
+        <h1>Counter : {counter}</h1>
+        <button onClick={Adding}>Increase</button>
+        <button onClick={Removing}>Decrease</button>
+    </div>
+  )
+}
+
+export default Counter
