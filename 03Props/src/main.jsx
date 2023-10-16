1172d65471724c254d6896e20b0f203dad24c8ed
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2023-10-16 15:25:53 +0500

    we  create card now we give a props to a Card

---
+++ 03Props/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Card from './Components/Card'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <Card />
+  </React.StrictMode>,
+)
